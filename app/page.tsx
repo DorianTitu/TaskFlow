305fd8c556ba5b39bf20ef0e7ab5fe3f7a6f6e17
Dorian Alexander Tituaña Pulupa <dorian.tituana@epn.edu.ec>
2024-11-06 09:49:32 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Este es un mensaje modificado</li>
+          <li>Este tambien es un mensaje que agregue dentro del despliegue de la aplicación</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
