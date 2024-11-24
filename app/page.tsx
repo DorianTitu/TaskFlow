bfa32a8863628650b2734086a0ee1b89c79a8f5c
Dorian Alexander Tituaña Pulupa <dorian.tituana@epn.edu.ec>
2024-11-24 06:00:05 -0500

    Modificacion de ejemplo

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
           <li>Este es un mensaje modificado</li>
           <li>Este tambien es un mensaje que agregue dentro del despliegue de la aplicación</li>
           <li>Este es un mensaje para desplegar</li>
+          <li>Tambien es un mensaje de prueba</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
